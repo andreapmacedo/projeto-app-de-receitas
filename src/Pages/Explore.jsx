bbab11f677fbab99f6aef1a3126c62d0ceda3540
andreapmacedo <aa.andremacedo@gmail.com>
2022-07-31 19:31:17 -0300

    feito: acrescenta palheta de cores

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-// import './Foods.css';
 import './Explore.css';
 
 function Explore() {
   return (
     <div className="explore">
-      <Header />
-      <Link to="/explore/foods">
-        <button type="button" data-testid="explore-foods">
-          Explore Foods
-        </button>
-      </Link>
+      <div className='explore-header' >
+        <Header />
+      </div>
+      <div className="explore-content">
+        <Link to="/explore/foods">
+          <button type="button" data-testid="explore-foods">
+            Explore Foods
+          </button>
+        </Link>
 
-      <Link to="/explore/drinks">
-        <button type="button" data-testid="explore-drinks">
-          Explore Drinks
-        </button>
-      </Link>
+        <Link to="/explore/drinks">
+          <button type="button" data-testid="explore-drinks">
+            Explore Drinks
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
