ab98e799280a75baa73e940d9f1ba414c461aa5b
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-07 00:06:06 -0300

    feito: adiciona estilo para inputs

--- src/Pages/ExploreNationalitiesDrinks.jsx
+++ src/Pages/ExploreNationalitiesDrinks.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
+import './Explore.css';
 
 function ExploreNationalitiesDrinks() {
   return (
-    <div>
-      <Header />
+    <div className="explore">
+      <div className="bcl"></div>
+      <div className='explore-header' >
+        <Header />
+      </div>
       <h3>Not Found</h3>
       <Footer />
     </div>
